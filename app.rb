dbff47ebdfc1d9424af02fcd69072bca314c59aa
briankim <brianhkim13@gmail.com>
2025-10-26 12:45:07 -0500

    updated meme app

--- app.rb
+++ app.rb
@@ -1,40 +1,62 @@
-require 'sinatra'
-require 'sinatra/reloader' if development?
+require 'sinatra/base'
+require 'sinatra/reloader'
+require 'yaml'
 
-# Sample meme data
-MEMES = {
-  funny: [
-    { title: "WaterBottleSchoolBoy", file: "funny1.jpeg" },
-    { title: "Birthyear", file: "funny2.jpeg" }
-  ],
-  wholesome: [
-    { title: "RiskyJokes", file: "wholesome1.jpeg" },
-    { title: "Finding a job", file: "wholesome2.jpeg" }
-  ],
-  dank: [
-    { title: "Loading chunks of trees", file: "dank1.jpeg" },
-    { title: "AWS-East-1", file: "dank2.jpeg" }
-  ]
-}
+class MemeExplorer < Sinatra::Base
+  configure :development do
+    register Sinatra::Reloader
+  end
 
-# Homepage
-get '/' do
-  erb :index
-end
+  MEMES = YAML.load_file("./data/memes.yml").transform_keys(&:to_s)
 
-# Category page
-get '/category/:name' do
-  @category_name = params[:name].to_sym
-  @memes = MEMES[@category_name]
-  halt 404, "Category not found" unless @memes
-  erb :category
-end
+  
+  VIEW_COUNTS ||= Hash.new(0)
 
-# Meme page
-get '/category/:category_name/meme/:title' do
-  category = params[:category_name].to_sym
-  @meme = MEMES[category].find { |m| m[:title] == params[:title] }
-  halt 404, "Meme not found" unless @meme
-  @category_name = category
-  erb :meme
+  get '/' do
+    erb :index
+  end
+
+  get '/category/:name' do
+    @category_name = params[:name]         # string keys
+    @memes = MEMES[@category_name]
+    halt 404, "Category not found" unless @memes
+    erb :category
+  end
+  
+  
+  
+  get '/random' do
+    category, memes = MEMES.to_a.sample
+    @meme = memes.sample
+    @category_name = category
+    erb :meme
+  end
+
+  get '/search' do
+    query = params[:q].to_s.downcase
+    @results = MEMES.flat_map do |category, memes|
+      memes.select { |m| m[:title].downcase.include?(query) }
+           .map { |m| [category, m] }
+    end
+    erb :search_results
+  end
+
+
+  get '/category/:category/meme/:title' do
+    @category_name = params[:category]     # string keys
+    category_memes = MEMES[@category_name]
+    halt 404, "Category not found" unless category_memes
+  
+    title = URI.decode_www_form_component(params[:title])
+    @meme = category_memes.find { |m| m["title"] == title }
+    halt 404, "Meme not found" unless @meme
+  
+    VIEW_COUNTS[@meme["title"]] += 1
+    @views = VIEW_COUNTS[@meme["title"]]
+    erb :meme
+  end
+  
+  
+
+  run! if app_file == $0
 end
