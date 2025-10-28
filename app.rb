c5640dfd75270b5b7a48ce59877d8e3d19b438e0
briankim <brianhkim13@gmail.com>
2025-10-27 20:21:36 -0500

    added more variety to the memes

--- app.rb
+++ app.rb
@@ -12,17 +12,47 @@ class MemeExplorer < Sinatra::Base
 
   MEMES = YAML.load_file("./data/memes.yml").transform_keys(&:to_s)
   VIEW_COUNTS ||= Hash.new(0)
-
   enable :sessions
 
-  POPULAR_SUBREDDITS = %w[dankmemes memes wholesomeMemes me_irl ProgrammerHumor].freeze
+  # Expanded subreddit pool for more variety
+  ALL_SUBREDDITS = %w[
+    dankmemes memes wholesomeMemes me_irl ProgrammerHumor
+    funny adviceanimals comedyheaven historymemes wholesomememes
+    starterpacks prequelmemes okbuddyretard memeEconomy
+    surrealmemes pewdiepieSubmissions ITMemes TerribleFacebookMemes
+    gamingmemes PoliticalHumor antiwork nextfuckinglevel
+    WhitePeopleTwitter BlackPeopleTwitter teenagers
+    cats memes_of_the_dank dogmemes wholesomeanimemes
+  ].freeze
 
-  # Home
+  POPULAR_SUBREDDITS = ALL_SUBREDDITS.sample(8)
+  MEME_CACHE = { memes: [], fetched_at: Time.at(0) }
+
+  # =========================
+  # Cookie Setup for Seen Memes
+  # =========================
+  before do
+    seen_cookie = request.cookies["seen_memes"]
+    @seen_memes = seen_cookie ? JSON.parse(seen_cookie) : []
+  end
+
+  after do
+    response.set_cookie("seen_memes", {
+      value: @seen_memes.to_json,
+      path: "/",
+      expires: Time.now + 60*60*24*30, # 30 days
+      httponly: true
+    })
+  end
+
+  # =========================
+  # Routes
+  # =========================
+
   get '/' do
     erb :index
   end
 
-  # Category page
   get '/category/:name' do
     @category_name = params[:name]
     @memes = MEMES[@category_name]
@@ -30,24 +60,25 @@ class MemeExplorer < Sinatra::Base
     erb :category
   end
 
-  # Random meme
   get '/random' do
-    session[:seen_memes] ||= []
-
-    memes = fetch_fresh_memes.reject { |m| session[:seen_memes].include?(m["url"]) }
+    memes = random_meme_pool.reject { |m| @seen_memes.include?(m["url"] || m["file"]) }
 
     if memes.empty?
-      # fallback to local memes
       category, local_memes = MEMES.to_a.sample
       memes = local_memes
       @category_name = category
     else
-      @category_name = "Reddit Memes"
+      @category_name = "Reddit & Local Memes"
     end
 
     @meme = memes.sample
-    @image_src = @meme["url"] || @meme["file"]  # <--- unified key
-    session[:seen_memes] << @meme["url"] if @meme["url"]
+    @image_src = @meme["url"] || @meme["file"]
+
+    seen_key = @meme["url"] || @meme["file"]
+    if seen_key
+      @seen_memes << seen_key
+      @seen_memes = @seen_memes.last(100) # keep only last 100 seen
+    end
 
     VIEW_COUNTS[@meme["title"]] += 1
     @views = VIEW_COUNTS[@meme["title"]]
@@ -55,7 +86,6 @@ class MemeExplorer < Sinatra::Base
     erb :meme
   end
 
-  # Search memes
   get '/search' do
     query = params[:q].to_s.downcase
     @results = MEMES.flat_map do |category, memes|
@@ -65,7 +95,6 @@ class MemeExplorer < Sinatra::Base
     erb :search_results
   end
 
-  # Single meme from category
   get '/category/:category/meme/:title' do
     @category_name = params[:category]
     category_memes = MEMES[@category_name]
@@ -75,20 +104,18 @@ class MemeExplorer < Sinatra::Base
     @meme = category_memes.find { |m| m["title"] == title }
     halt 404, "Meme not found" unless @meme
 
-    @image_src = @meme["url"] || @meme["file"]  # <--- unified key
+    @image_src = @meme["url"] || @meme["file"]
     VIEW_COUNTS[@meme["title"]] += 1
     @views = VIEW_COUNTS[@meme["title"]]
 
     erb :meme
   end
 
-  # API memes page
   get '/api_memes' do
     @api_memes = fetch_api_memes
     erb :api_memes
   end
 
-  # Like/unlike meme
   post '/like' do
     content_type :json
     begin
@@ -110,29 +137,66 @@ class MemeExplorer < Sinatra::Base
     end
   end
 
-  # Fetch fresh memes from Reddit API
-  def fetch_fresh_memes
-    subreddit = POPULAR_SUBREDDITS.sample
-    url = URI("https://meme-api.com/gimme/#{subreddit}/10")
+  # =========================
+  # Helpers
+  # =========================
 
+  def random_meme_pool
+    api_memes = fetch_fresh_memes(20)
+    global_randoms = fetch_global_randoms(20)
+    local_memes = MEMES.values.flatten.sample(10)
+    (api_memes + global_randoms + local_memes).shuffle.uniq { |m| m["url"] || m["file"] }
+  end
+
+  # Fetch memes from selected subreddits with multiple calls per subreddit
+  def fetch_fresh_memes(batch_size = 20)
+    if Time.now - MEME_CACHE[:fetched_at] > 120 # refresh every 2 min
+      MEME_CACHE[:memes] = get_memes_from_reddit(batch_size)
+      MEME_CACHE[:fetched_at] = Time.now
+    end
+    MEME_CACHE[:memes]
+  end
+
+  def get_memes_from_reddit(batch_size = 20)
+    memes = []
+
+    POPULAR_SUBREDDITS.sample(8).each do |subreddit|
+      2.times do
+        url = URI("https://meme-api.com/gimme/#{subreddit}/#{batch_size}")
+        begin
+          response = Net::HTTP.get(url)
+          data = JSON.parse(response)
+          new_memes = data["memes"] || []
+          memes.concat(new_memes.map do |m|
+            {
+              "title" => m["title"],
+              "url" => m["url"],
+              "postLink" => m["postLink"],
+              "subreddit" => m["subreddit"]
+            }
+          end)
+        rescue => e
+          puts "API error for #{subreddit}: #{e.message}"
+          next
+        end
+      end
+    end
+
+    memes.uniq { |m| m["url"] }
+  end
+
+  # Fetch global random memes to increase variety
+  def fetch_global_randoms(count = 20)
+    url = URI("https://meme-api.com/gimme/#{count}")
     response = Net::HTTP.get(url)
     data = JSON.parse(response)
-    memes = data["memes"] || []
-
-    memes.map do |m|
-      {
-        "title" => m["title"],
-        "url" => m["url"],
-        "postLink" => m["postLink"],
-        "subreddit" => m["subreddit"]
-      }
+    (data["memes"] || [data]).compact.map do |m|
+      { "title" => m["title"], "url" => m["url"], "postLink" => m["postLink"], "subreddit" => m["subreddit"] }
     end
-  rescue StandardError => e
-    puts "API Error in fetch_fresh_memes: #{e.message}"
+  rescue
     []
   end
 
-  # Fetch multiple API memes
   def fetch_api_memes(count = 18)
     url = URI("https://meme-api.com/gimme/#{count}")
     response = Net::HTTP.get(url)
