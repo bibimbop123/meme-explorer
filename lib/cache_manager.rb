a9bd0782c3d1599f212ebc7becc4c0b5c7c97fcc
briankim <brianhkim13@gmail.com>
2025-11-03 02:29:13 -0600

    done

--- lib/cache_manager.rb
+++ lib/cache_manager.rb
@@ -1,99 +1,112 @@
-# Meme Explorer Cache Manager - LRU cache with memory limits
-class MemeExplorerCacheManager
-  def initialize(max_size_bytes, ttl_seconds)
-      @max_size = max_size_bytes
-      @ttl = ttl_seconds
-      @cache = {}
-      @timestamps = {}
-      @access_times = {}
-      @mutex = Mutex.new
-      @current_size = 0
-    end
+require 'monitor'
 
-    def set(key, value)
-      @mutex.synchronize do
-        if @cache[key]
-          @current_size -= estimate_size(@cache[key])
-        end
+class CacheManager
+  @@cache = {}
+  @@cache_lock = Monitor.new
+  @@cache_timestamps = {}
+  @@cache_hit_count = {}
+  MAX_CACHE_SIZE = 100 * 1024 * 1024
 
-        @cache[key] = value
-        @timestamps[key] = Time.now
-        @access_times[key] = Time.now
-        new_size = estimate_size(value)
-        @current_size += new_size
-
-        evict_if_needed
-      end
-    end
-
+  class << self
     def get(key)
-      @mutex.synchronize do
-        return nil unless @cache[key]
-        if Time.now - @timestamps[key] > @ttl
-          delete(key)
-          return nil
+      @@cache_lock.synchronize do
+        if @@cache.key?(key)
+          @@cache_hit_count[key] = (@@cache_hit_count[key] || 0) + 1
+          return @@cache[key]
         end
-        @access_times[key] = Time.now
-        @cache[key]
+      end
+      nil
+    end
+
+    def set(key, value, ttl = 3600)
+      @@cache_lock.synchronize do
+        if should_evict?
+          evict_lru
+        end
+        @@cache[key] = value
+        @@cache_timestamps[key] = Time.now
+        @@cache_hit_count[key] = 0
       end
     end
 
     def delete(key)
-      @mutex.synchronize do
-        if @cache[key]
-          @current_size -= estimate_size(@cache[key])
-          @cache.delete(key)
-          @timestamps.delete(key)
-          @access_times.delete(key)
-        end
+      @@cache_lock.synchronize do
+        @@cache.delete(key)
+        @@cache_timestamps.delete(key)
+        @@cache_hit_count.delete(key)
       end
     end
 
     def clear
-      @mutex.synchronize do
-        @cache.clear
-        @timestamps.clear
-        @access_times.clear
-        @current_size = 0
+      @@cache_lock.synchronize do
+        @@cache.clear
+        @@cache_timestamps.clear
+        @@cache_hit_count.clear
+      end
+    end
+
+    def size
+      @@cache_lock.synchronize do
+        @@cache.size
       end
     end
 
     def stats
-      @mutex.synchronize do
+      @@cache_lock.synchronize do
         {
-          size: @current_size,
-          max_size: @max_size,
-          used_percent: (@current_size.to_f / @max_size * 100).round(2),
-          entries: @cache.size,
-          ttl: @ttl
+          size: @@cache.size,
+          estimated_memory: estimate_size,
+          keys: @@cache.keys
         }
       end
     end
 
     private
 
-    def estimate_size(value)
-      case value
-      when String
-        value.bytesize
-      when Array
-        value.sum { |v| estimate_size(v) }
-      when Hash
-        value.sum { |k, v| estimate_size(k) + estimate_size(v) }
-      else
-        ObjectSpace.memsize_of(value)
-      end
+    def should_evict?
+      estimate_size > MAX_CACHE_SIZE
     end
 
-    def evict_if_needed
-      while @current_size > @max_size && !@cache.empty?
-        lru_key = @access_times.min_by { |k, v| v }[0]
-        delete(lru_key)
+    def evict_lru
+      return if @@cache.empty?
+      lru_key = @@cache.keys.min_by do |key|
+        hit_count = @@cache_hit_count[key] || 0
+        timestamp = @@cache_timestamps[key] || Time.now
+        [hit_count, timestamp.to_i]
       end
+      @@cache.delete(lru_key)
+      @@cache_timestamps.delete(lru_key)
+      @@cache_hit_count.delete(lru_key)
+    end
 
-      now = Time.now
-      @cache.each_key do |key|
-        delete(key) if now - @timestamps[key] > @ttl
+    def estimate_size
+      total_size = 0
+      @@cache.each do |key, value|
+        total_size += key.to_s.bytesize
+        total_size += estimate_object_size(value)
       end
+      total_size
+    rescue => e
+      @@cache.size * 10_000
+    end
+
+    def estimate_object_size(obj)
+      case obj
+      when String
+        obj.bytesize + 40
+      when Array
+        (obj.map { |item| estimate_object_size(item) }.sum) + 40
+      when Hash
+        (obj.sum { |k, v| estimate_object_size(k) + estimate_object_size(v) }) + 40
+      when Integer, Float, TrueClass, FalseClass, NilClass
+        24
+      when Symbol
+        obj.to_s.bytesize + 40
+      else
+        100
+      end
+    rescue => e
+      100
+    end
   end
 end
