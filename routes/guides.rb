a2f4b5067920723adf3e6aae32d37d99d80696ad
briankim <brianhkim13@gmail.com>
2026-07-07 20:56:10 -0500

    Add educational guides for AdSense approval
    - 11 pages of original educational content (6,000+ words)
    - Fixed Routes::Guides module structure
    - Demonstrates expertise in quality systems and curation
    - Improves user onboarding and feature discovery

--- routes/guides.rb
+++ routes/guides.rb
@@ -4,51 +4,55 @@
 # Created: July 7, 2026
 # Purpose: Educational guides demonstrating expertise in meme curation
 
-class MemeExplorer < Sinatra::Base
-  # Guide index page
-  get '/guides' do
-    erb :guides_index
-  end
+module Routes
+  module Guides
+    def self.registered(app)
+      # Guide index page
+      app.get '/guides' do
+        erb :guides_index
+      end
 
-  # Core Feature Guides
-  get '/guides/quality-system' do
-    erb :'guides/quality_system'
-  end
+      # Core Feature Guides
+      app.get '/guides/quality-system' do
+        erb :'guides/quality_system'
+      end
 
-  get '/guides/personalization' do
-    erb :'guides/personalization'
-  end
+      app.get '/guides/personalization' do
+        erb :'guides/personalization'
+      end
 
-  get '/guides/gamification' do
-    erb :'guides/gamification'
-  end
+      app.get '/guides/gamification' do
+        erb :'guides/gamification'
+      end
 
-  get '/guides/collections' do
-    erb :'guides/collections'
-  end
+      app.get '/guides/collections' do
+        erb :'guides/collections'
+      end
 
-  get '/guides/discovery' do
-    erb :'guides/discovery'
-  end
+      app.get '/guides/discovery' do
+        erb :'guides/discovery'
+      end
 
-  # User Onboarding Guides
-  get '/guides/getting-started' do
-    erb :'guides/getting_started'
-  end
+      # User Onboarding Guides
+      app.get '/guides/getting-started' do
+        erb :'guides/getting_started'
+      end
 
-  get '/guides/meme-formats' do
-    erb :'guides/meme_formats'
-  end
+      app.get '/guides/meme-formats' do
+        erb :'guides/meme_formats'
+      end
 
-  get '/guides/best-practices' do
-    erb :'guides/best_practices'
-  end
+      app.get '/guides/best-practices' do
+        erb :'guides/best_practices'
+      end
 
-  get '/guides/community' do
-    erb :'guides/community'
-  end
+      app.get '/guides/community' do
+        erb :'guides/community'
+      end
 
-  get '/guides/faq' do
-    erb :'guides/faq'
+      app.get '/guides/faq' do
+        erb :'guides/faq'
+      end
+    end
   end
 end
